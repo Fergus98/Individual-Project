bf26b6e52a5d63523eefd638d52e7e2fc9cce106
Fergus98 <Fergus98@users.noreply.github.com>
2020-03-07 14:52:35 +0000

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -5,9 +5,9 @@ from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+app.config['SQLALCHEMY_DATABASE_URI'] = str(getenv('DATABASE_URI'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = str(os.getenv('SECRET_KEY'))
+app.config['SECRET_KEY'] = str(getenv('SECRET_KEY'))
 db = SQLAlchemy(app)
 
 from application import routes
