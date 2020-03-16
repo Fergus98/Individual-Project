b0ca5f96f970fe43fed6e6aba9715a79b7e8676a
Ubuntu <fergus@jenkins-manager.bjrnbrgbyrmuxhizah2fjfuq1b.zx.internal.cloudapp.net>
2020-03-16 16:44:00 +0000

    SQLIMAGE

---
+++ sqlimage/Create.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE baseball;
+CREATE table baseball.game(
+	game_no INTEGER NOT NULL PRIMARY KEY,
+	losing_team STRING(30) NOT NULL,
+	winning_team STRING(30) NOT NULL,
+	score INTEGER NOT NULL
+);
+
+CREATE table baseball.team(
+	team_id INTEGER NOT NULL PRIMARY KEY,
+	wins INTEGER NOT NULL,
+	losses INTEGER NOT NULL
+);
+
+CREATE table baseball.players(
+	player_id INTEGER NOT NULL PRIMARY KEY,
+	team1 = STRING(30) NOT NULL,
+	player_name STRING(30) NOT NULL
+);
